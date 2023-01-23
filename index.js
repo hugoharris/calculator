5634f7d457a5a0901cfa4c51ba2a007b9467ec2c
Kevin Harris <hugokharris@gmail.com>
2023-01-23 13:46:54 -0700

    add DOM elements to index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+// DOM elements
+// numbers
+const oneBtn = document.querySelector('.one');
+const twoBtn = document.querySelector('.two');
+const threeBtn = document.querySelector('.three');
+const fourBtn = document.querySelector('.four');
+const fiveBtn = document.querySelector('.five');
+const sixBtn = document.querySelector('.six');
+const sevenBtn = document.querySelector('.seven');
+const eightBtn = document.querySelector('.eight');
+const nineBtn = document.querySelector('.nine');
+// operators
+const divideBtn = document.querySelector('.divide');
+const multiplyBtn = document.querySelector('.multiply');
+const subtractBtn = document.querySelector('.subtract');
+const addBtn = document.querySelector('.add');
+// other elements
+const screen = document.querySelector('.screen');
+const clearBtn = document.querySelector('.clear');
+const deleteBtn = document.querySelector('.delete');
+const decimalBtn = document.querySelector('.decimal');
+const equalsBtn = document.querySelector('.equals');
+
