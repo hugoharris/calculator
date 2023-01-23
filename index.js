7b8db826c722a537ddf7143298ab571a54bb0372
Kevin Harris <hugokharris@gmail.com>
2023-01-23 15:34:26 -0700

    added all basic functions including BigEval computation module

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // DOM elements
 // numbers
+const zeroBtn = document.querySelector('.zero');
 const oneBtn = document.querySelector('.one');
 const twoBtn = document.querySelector('.two');
 const threeBtn = document.querySelector('.three');
@@ -22,31 +23,57 @@ const decimalBtn = document.querySelector('.decimal');
 const equalsBtn = document.querySelector('.equals');
 
 // functions
+function calculateAnswer(passedInArray) {
+    const Obj = new BigEval();
+    return Obj.exec(passedInArray.join(''));
+}
+
 function handleNumberClick(numStr) {
+    const calcStr = calcArray[calcArray.length - 1];
+    if (calcStr === "+" || calcStr === "-" || calcStr === "/" || calcStr === "*") {
+        if (calcArrayToggle === true) {
+            calcArrayToggle = false;
+            resetScreen();
+        }
+    }
     if (screenDisplay === '0') {
         screenDisplay = numStr;
-        currentNum = 1;
+        currentNum = Number(numStr);
         updateScreen(screenDisplay);
-    } else {
+    } else if (screenDisplay.length < maxCharLength) {
         screenDisplay += numStr;
         currentNum = Number(screenDisplay);
         updateScreen(screenDisplay);
     }
 }
 
+function handleOperatorClick(op) {
+    if (calcArrayToggle === false) {
+        calcArray.push(currentNum);
+        calcArray.push(op);
+        calcArrayToggle = true;
+        console.log(calcArray);
+        currentNum = 0;
+    }
+}
+
 function resetScreen() {
-    currentNum = 0;
     screenDisplay = '0';
+    currentNum = 0;
     updateScreen(screenDisplay);
 }
 
 function updateScreen(screenDisplay) {
     screen.innerHTML = `<p>${screenDisplay}</p>`
+    console.log(calcArray, currentNum);
 }
 
 // set default variables
 let screenDisplay = '0';
 let currentNum = 0;
+let calcArray =[];
+let calcArrayToggle = false;
+const maxCharLength = 12;
 
 // reset screen on first run
 resetScreen();
@@ -55,31 +82,76 @@ resetScreen();
 
 // event listeners - clear and delete
 clearBtn.addEventListener('click', () => {
+    calcArray = [];
+    calcArrayToggle = false;
     resetScreen();
 });
 
 deleteBtn.addEventListener('click', () => {
-    if (screenDisplay !== '0') {
-        if (screenDisplay.length === 1) {
-            screenDisplay = '0';
-            currentNum = 0;
-            updateScreen(screenDisplay);
-        } else {
-            screenDisplay = screenDisplay.slice(0, -1);
-            currentNum = Number(screenDisplay);
-            updateScreen(screenDisplay);
-        }
-    } 
+    if (typeof calcArray[calcArray.length - 1] === "number" || calcArray.length === 0) {
+        if (screenDisplay !== '0') {
+            if (screenDisplay.length === 1) {
+                screenDisplay = '0';
+                currentNum = 0;
+                updateScreen(screenDisplay);
+            } else {
+                screenDisplay = screenDisplay.slice(0, -1);
+                currentNum = Number(screenDisplay);
+                updateScreen(screenDisplay);
+            }
+        } 
+    }
 });
 
 // event listeners - numbers
-const numberButtons = [oneBtn, twoBtn, threeBtn, fourBtn, fiveBtn, sixBtn, sevenBtn, eightBtn, nineBtn];
+const numberButtons = [zeroBtn, oneBtn, twoBtn, threeBtn, fourBtn, fiveBtn, sixBtn, sevenBtn, eightBtn, nineBtn];
 for (let numberBtn of numberButtons) {
     numberBtn.addEventListener('click', (e) => {
         const numStr = e.target.innerText;
         handleNumberClick(numStr);
     });
 }
+
+// event listener - decimal
+decimalBtn.addEventListener('click', (e) => {
+    if (screenDisplay.indexOf('.') === -1) {
+        const numStr = e.target.innerText;
+        handleNumberClick(numStr);
+    }
+});
+
+// event listeners - operators
+addBtn.addEventListener('click', () => {
+    handleOperatorClick('+');
+});
+
+subtractBtn.addEventListener('click', () => {
+    handleOperatorClick('-');
+});
+
+multiplyBtn.addEventListener('click', () => {
+    handleOperatorClick('*');
+});
+
+divideBtn.addEventListener('click', () => {
+    handleOperatorClick('/');
+});
+
+// event listener - equals
+
+equalsBtn.addEventListener('click', () => {
+    if (calcArray.length === 0) {
+        calcArray = [];
+        calcArrayToggle = false;
+    } else {
+        calcArray.push(currentNum);
+        const answer = calculateAnswer(calcArray);
+        screenDisplay = answer;
+        currentNum = Number(screenDisplay);
+        calcArray = [];
+        updateScreen(screenDisplay);
+    }
+});
 
 
 
